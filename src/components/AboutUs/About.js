936cca1d73e9979df883ba82c5c737f36b037096
kevin-bayat <kevin.bayat@yahoo.com>
2022-04-06 22:18:44 +0430

    add translate button into about us section

--- src/components/AboutUs/About.js
+++ src/components/AboutUs/About.js
@@ -3,8 +3,10 @@ import images from "../../constants/images";
 import SubHeading from "../SubHeading/SubHeading";
 import { Link } from "react-router-dom";
 import Button from "../Button/Button";
+import { useState } from "react";
 
 const About = () => {
+  const [isTranslate, setIsTranslate] = useState(false);
   return (
     <div className="app_aboutUs flex__center section__padding" id="about-us">
       <div className="app_aboutUs-image">
@@ -17,15 +19,34 @@ const About = () => {
           kind="studio"
           color="#0c0c0c"
         />
-        <p className="p__openSans" style={{ color: "#0c0c0c" }}>
-          Ad iusto esse eum maiores sunt sed doloremque sunt vel expedita
-          molestiae quo laboriosam dolorem sit
-        </p>
-        <p className="p__openSans" style={{ color: "#545454" }}>
-          Lorem ipsum dolor sit amet. Est nisi itaque et aperiam debitis non
-          numquam fugit ad sunt veritatis cum molestias consequatur et culpa
-          ratione. Sed exercitationem molestias aut explicabo alias eum
-        </p>
+        {!isTranslate ? (
+          <>
+            <p className="p__openSans" style={{ color: "#0c0c0c" }}>
+              Ad iusto esse eum maiores sunt sed doloremque sunt vel expedita
+              molestiae quo laboriosam dolorem sit
+            </p>
+            <p className="p__openSans" style={{ color: "#545454" }}>
+              Lorem ipsum dolor sit amet. Est nisi itaque et aperiam debitis non
+              numquam fugit ad sunt veritatis cum molestias consequatur et culpa
+              ratione. Sed exercitationem molestias aut explicabo alias eum
+            </p>
+          </>
+        ) : (
+          <p className="p__iranSans" style={{ color: "#545454" }}>
+            معماری نقاط مختلف آسیا در مسیری جدای از مسیر اروپا توسعه یافت؛
+            معماری بودایی، هندویی و سیکی هر کدام ویژگی‌های منحصربفرد خود را
+            دارند. به ویژه معماری بودایی، تنوع منطقه ای بسیار خوبی از خود نشان
+            می‌دهد. در بسیاری از کشورهای آسیایی با مذاهب پانتزیستی، معماری به
+            سمت بهبود چشم‌انداز طبیعی گرایش زیادی داشته‌است.
+          </p>
+        )}
+        <Button
+          className="custom__button"
+          onClick={() => setIsTranslate(!isTranslate)}
+          style={{ marginRight: "15px" }}
+        >
+          Translate
+        </Button>
         <Link to="/about-us">
           <Button className="custom__button">Read More</Button>
         </Link>
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Button.styles.scss";
 
-const Button = ({ children, ...props }) => (
-  <button className="custom__button">{children}</button>
+const Button = ({ children, onClick, ...props }) => (
+  <button className="custom__button" onClick={onClick} {...props}>
+    {children}
+  </button>
 );
 export default Button;
